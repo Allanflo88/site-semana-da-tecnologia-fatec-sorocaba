052ef8de58647c8bc5870f18b8f765d260613449
Allanflo88 <allanfloresdejesus@hotmail.com>
2018-07-05 11:29:35 -0300

    remove unused dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 var gulp = require("gulp");
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var browserSync = require('browser-sync').create();
 
 gulp.task('sass', () => {
     return gulp.src('./*.+(scss|sass)')
@@ -10,20 +9,7 @@ gulp.task('sass', () => {
         .pipe(sass())
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./'))
-        .pipe(browserSync.stream());
     }
 );
 
-gulp.task('browserSync', gulp.series(['sass']), () => {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        },
-        port: 8080,
-        startPath: "index.html"
-    });
-
-    gulp.watch('style.scss', ['sass']);
-});
-
-gulp.task('watch', gulp.series('browserSync'));
+gulp.task('default', gulp.series('sass'));
