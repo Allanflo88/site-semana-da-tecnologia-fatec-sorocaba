8a3c2c80ca49895f6ca1759713c557e9c70c059c
Allanflo88 <allanfloresdejesus@hotmail.com>
2018-07-12 22:05:07 -0300

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,23 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', () => {
-    return gulp.src('./*.+(scss|sass)')
+    return gulp.src('./style.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./'))
+        .pipe(gulp.dest('./dist'))
     }
 );
 
-gulp.task('default', gulp.series('sass'));
+gulp.task('html',()=>{
+    return gulp.src('./index.html')
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('javascript',()=>{
+    return gulp.src('./script.js')
+        .pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('default', gulp.series('html','sass','javascript'));
