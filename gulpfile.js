94d686430d07c1583b12b79e02342241bdb0d346
Allanflo88 <allanfloresdejesus@hotmail.com>
2018-08-08 13:15:13 -0300

    update gulpfile to builds

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,24 @@ var gulp        = require("gulp");
     sass        = require("gulp-sass"),
     sourcemaps  = require("gulp-sourcemaps"),
     browserSync = require("browser-sync").create(),
+    cleanCSS    = require("gulp-clean-css"),
     reload      = browserSync.reload;
+gulp.task("minify-css", () => {
+    return gulp.src("./style.css")
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("./build"));
+});
+gulp.task("js", ()=>{
+    return gulp.src("./script.js")
+        .pipe(gulp.dest("./build"));
+})
+gulp.task("html", ()=>{
+    return gulp.src("./index.html")
+        .pipe(gulp.dest("./build"));
+})
+
 gulp.task("sass", () => {
   return gulp
     .src("./style.scss")
@@ -23,5 +40,28 @@ gulp.task('serve', function () {
     gulp.watch("*.js").on("change", reload);
 
 });
+
+gulp.task("svg", () => {
+    return gulp.src(["./images/*.*"])
+        .pipe(gulp.dest("./build/images"));
+})
+gulp.task("sponsors", () => {
+    return gulp.src(["./images/sponsors/*.png"])
+        .pipe(gulp.dest("./build/images/sponsors"));
+})
+gulp.task("previous", () => {
+    return gulp.src(["./images/previous/*.jpeg"])
+        .pipe(gulp.dest("./build/images/previous"));
+})
+gulp.task("organizers", () => {
+    return gulp.src(["./images/organizers/**.jpeg"])
+        .pipe(gulp.dest("./build/images/organizers"));
+})
+gulp.task("icons", () => {
+    return gulp.src(["./images/favicons/*.ico"])
+        .pipe(gulp.dest("./build/images/favicons"));
+})
+
+gulp.task("build",gulp.series("html", "minify-css", "js","svg", "sponsors","previous", "organizers", "icons"));
 
 gulp.task("default", gulp.series("sass", "serve"));
